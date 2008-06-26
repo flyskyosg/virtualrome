c770ab471bd755508b384d9684c77498e469c556
luigi.calori <luigi.calori@363b1339-bb4f-0410-9e72-7d383497cecc>
2008-06-26 17:44:42 +0000

    added test with wxviewer

--- py_code/py25_apps/code/VirtualRome/pick_module.py
+++ py_code/py25_apps/code/VirtualRome/pick_module.py
@@ -1,11 +1,11 @@
-import osg_setup
+#import osg_setup
 import osg
 import osgDB
 import osgGA
 import osgViewer
 import osgUtil
 
-tex=0
+
 
 def createWireBox(node):
     """create a bounding box for the node """
@@ -44,7 +44,7 @@ def createWireBox(node):
     lw.setWidth(2.0)
     stateset.setAttribute(lw)
     
-    stateset.setAttribute(selmat)
+    #stateset.setAttribute(selmat)
 
     #create a drawablw box with the right position and size
     lx = bb._max.x() - bb._min.x()
@@ -56,12 +56,225 @@ def createWireBox(node):
     
     #add the drawable to the wirebox geode
     wbgeode.addDrawable(shape)
+    wbgeode.setName("Wirebox")
 
     for pointer in [stateset, box, polyModeObj, lw, shape]:
         pointer.thisown = False
 
     #return the wirebox geode
     return wbgeode
+
+def wireboxed(node):
+    g=osg.Group()
+    g.addChild(node)
+    g.addChild(createWireBox(node))
+    g.thisown=False
+    return g
+
+def downsize(node):
+    g=osg.MatrixTransform()
+    m=osg.Matrixd()
+    m.setTrans(0.0,0.0,10.0)
+    g.addChild(node)
+    g.setMatrix(m)
+    g.thisown=False
+    m.thisown=False
+    return g
+
+def replace_nodes((oldnode,newnode,parents)):
+##    print "in replace nodes, parentslist-->",parents.__len__()
+##    print "oldnode-->",oldnode
+##    print "newnode-->",newnode
+    for i in parents:
+##        print i
+        i.addChild(newnode)
+        i.removeChild(oldnode)
+        
+def getparents(node):
+    parents=[]
+##    print "getparents-->",node.getNumParents()
+    for i in range(0,node.getNumParents()):
+##        print i
+        parents.append(node.getParent(i))
+    return parents
+        
+class highlighter():
+    def __init__(self,hfun):
+        self.selected=dict()
+        self.hilight=hfun
+        self.count=0
+    def select(self,node):
+##        print "in select-->",node,node.getNumParents()
+        if(node):
+            if(node.getNumParents()):
+                if(self.selected.has_key(node.this.__hex__())):
+                    print "de-select-->",self.selected[node.this.__hex__()][1]
+                    replace_nodes(self.selected.pop(node.this.__hex__()))
+                    self.count=self.count -1
+                else:
+                    print "select-->",str(node.this.__hex__())
+                    parents=getparents(node)
+                    hilight=self.hilight(node)
+                    hilight.setName("selected_"+str(self.count))
+                    self.selected[hilight.this.__hex__()]=(hilight,node,parents)
+                    replace_nodes((node,hilight,parents))
+                    print "inserting selection-->",hilight.this.__hex__(),"<-->",self.count
+                    self.count=self.count +1
+
+    def is_hilit(self,node):
+        nodename=node.getName()
+        if(nodename):
+            nodename= node.this.__hex__() + "<-->" + nodename
+        else:
+            nodename= node.this.__hex__() 
+        print "testing hilit node-->",nodename,"<-->",self.selected.has_key(node.this.__hex__())
+        return self.selected.has_key(node.this.__hex__())
+
+class PickAction():
+    def __init__(self):
+        print "PickAction. __init__ "
+        self.start()
+    def handle(self,intersection):
+        self.start()
+        mypoint = intersection.getWorldIntersectPoint()
+        print "Intersection Point ->", mypoint.x(), mypoint.y(), mypoint.z()
+        for node in intersection.nodePath:
+            print node.className(), "<-->",node.getName()
+            if(self.match(node)):
+                if(self.do(node)):
+                    return True
+            else:
+                print "node -->",node.this.__hex__()," does not match"
+        return self.end()
+    
+    def match(self,node):
+        print "PickAction match "
+        if(not self.firstnode):
+            self.firstnode=node
+            print "setting firstnode-->",node
+
+    def do(self,node):
+        self.done=True
+        print "PickAction do "
+    def start(self):
+        self.firstnode=None
+        self.done=False
+
+    def end(self):
+        print "PickAction end "
+        return False
+    
+class PrintTextures(PickAction):
+    def __init__(self):
+        PickAction.__init__(self)
+        print "PrintTextures __init__ "
+    def match(self,node):
+        print "PrintTextures match "
+        self.st = node.getStateSet()
+        if self.st:
+            self.st.thisown = False
+            return True
+        else:
+            g = osg.NodeToGeode(node)
+            if g:
+                print "geode-->",g.getNumDrawables()
+                for j in range(0,g.getNumDrawables()):
+                    d  = g.getDrawable(j);
+                    self.st = d.getStateSet()
+                    if self.st:
+                        self.st.thisown = False
+                        return True
+            return False
+    
+    def do(self,node):
+        print "PrintTextures do "
+        for i in [0,1,2,3]:
+            tex = self.st.getTextureAttribute(i,osg.StateAttribute.TEXTURE)
+            if tex:
+                print "got texture channel", i, " -->",tex.className()
+        return False
+                
+
+class HiLight(PickAction):
+    def __init__(self,hl):
+        PickAction.__init__(self)
+        print "HiLight __init__ "
+        self.hl=hl
+    def match(self,node):
+        PickAction.match(self,node)
+        print "sono qui"
+        if(self.hl.is_hilit(node)):
+            return True
+        else:
+            g = osg.NodeToGeode(node)
+            if g:
+                self.firstnode=g
+            return False
+
+    
+    def do(self,node):
+        PickAction.do(self,node)
+        print "HiLight do "
+        self.hl.select(node)
+        return True
+ 
+    def end(self):
+        print "HiLight end -->",self.done
+        if(not self.done):
+            self.hl.select(self.firstnode)
+            return True
+        else:
+            return False
+               
+
+
+class PickHandlerBase(osgGA.GUIEventHandler): 
+    """PickHandler -- A GUIEventHandler that implements picking."""
+    base_class = osgGA.GUIEventHandler
+    def __init__(self,action):
+        #Store mouse xy location for button press and move events.
+        self._mX = 0
+        self._mY = 0   
+        self.action=action
+        osgGA.GUIEventHandler.__init__(self)
+    def handle(self, ea, aa, obj, nv):        
+        vwr = osgViewer.GUIActionAdapterToViewer(aa)
+        if not vwr:
+            return False
+        eventtype = ea.getEventType()
+        if eventtype == ea.PUSH or eventtype == ea.MOVE:
+            self._mX = ea.getX()
+            self._mY = ea.getY()
+            return False
+        if eventtype == ea.RELEASE:
+            print "mouse coord-->",self._mX, self._mY
+            #check if mouse didn't move (else let trackball manipulator handle it)
+            if self._mX == ea.getX() and self._mY == ea.getY():
+                if self.pick( ea.getXnormalized(), ea.getYnormalized(), vwr ):
+                    return True
+        return False
+    def pick(self, x, y , viewer):
+
+        print "Picking->",x,"<->",y 
+        if not viewer.getSceneData():
+            return False
+        #create an intersector
+        picker = osgUtil.LineSegmentIntersector(osgUtil.Intersector.PROJECTION, x, y )
+        #create an intersectionVisitor
+        iv = osgUtil.IntersectionVisitor( picker )
+        #visit the sceneGraph
+        viewer.getCamera().accept( iv )
+        #check for intersections
+        if picker.containsIntersections():
+            intersection = picker.getFirstIntersection()
+            self.action.handle(intersection)
+            return False                            
+        else:
+            print "No Intersection Found"            
+        return False       
+        
+
+
 
 
 class PickHandler(osgGA.GUIEventHandler): 
@@ -156,3 +369,65 @@ class PickHandler(osgGA.GUIEventHandler):
             print "No Intersection Found"            
         return False       
 
+###-------------------------------------------------------------------
+##if __name__ == "__main__":
+##    # create a viewer
+##    viewer = osgViewer.Viewer()
+##
+### configure
+##    viewer.setThreadingModel(osgViewer.Viewer.SingleThreaded)
+##
+### add to the scene
+##    root=osg.Group()
+##    n=osgDB.readNodeFile("D:/prove/oppidum/oppidum_opt._fwslash.osg")
+##    root.addChild(n)
+##    root.addChild(downsize(n))
+##    root.addChild(wireboxed(n))
+##    root.addChild(wireboxed(downsize(n)))
+##    viewer.setSceneData(root)
+##
+##    hlt=HiLight(highlighter(wireboxed))
+##    pickhandler1 = PickHandlerBase(hlt)
+##    
+##    viewer.addEventHandler(pickhandler1.__disown__());
+##
+### loop until done
+##    viewer.run()
+
+
+
+#--------------------------------------------------------------------------
+if __name__ == "__main__":
+    import wxosgviewer 
+    import os
+    
+    app = wxosgviewer.wxOsgApp(0)  # importante: creare APP passando 0 
+    viewer = app.frame.canvas.viewer
+    
+    # locate the DataDir
+    dir = os.getenv('DATADIR')
+    if not dir:
+        print 'env-var "DATADIR" not found, exiting'
+        sys.exit()
+    # there are good and bad data :-)
+    dir = dir + 'bad\\'
+
+    # open the test file
+    filename = dir + 'f_pace.osg'
+    node = osgDB.readNodeFile(filename)
+    if not node : 
+        print 'error loading', filename
+        sys.exit()
+    
+    viewer.setSceneData(node)
+
+    hlt=HiLight(highlighter(wireboxed))
+    pickhandler1 = PickHandlerBase(hlt)
+    
+    viewer.addEventHandler(pickhandler1.__disown__());
+
+    
+    
+    
+    
+    app.MainLoop()
