f6d6265bcf4f181ab04e6652abcbb76ef46537fb
luigi.calori <luigi.calori@363b1339-bb4f-0410-9e72-7d383497cecc>
2009-10-10 09:43:12 +0000

    interpolateCR change float to double

--- osg4web/bzr_import/include/CommonCore/SceneHandlers/AnimateViewHandler.h
+++ osg4web/bzr_import/include/CommonCore/SceneHandlers/AnimateViewHandler.h
@@ -51,7 +51,7 @@ namespace SceneHandlers
 		bool handle(const osgGA::GUIEventAdapter& ea,osgGA::GUIActionAdapter& aa);
 
 		// Catmull-Rom Interpolation (p1,p2)
-		osg::Vec3 interpolateCR(float t, osg::Vec3 p0, osg::Vec3 p1, osg::Vec3 p2, osg::Vec3 p3);
+		osg::Vec3 interpolateCR(double t, osg::Vec3 p0, osg::Vec3 p1, osg::Vec3 p2, osg::Vec3 p3);
 
 		//Ridefinizioni del Gestore dei Comandi
 		virtual std::string handleAction(std::string argument);
