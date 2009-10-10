c3bd019c754984ada172b3e2983a4522306c124a
luigi.calori <luigi.calori@363b1339-bb4f-0410-9e72-7d383497cecc>
2009-10-10 09:37:51 +0000

    *** empty log message ***

--- trunk/osg4web/bzr_import/src/Engines/LoaderOpenGL/include/LoaderOpenGL/LoaderOpenGL.h
+++ trunk/osg4web/bzr_import/src/Engines/LoaderOpenGL/include/LoaderOpenGL/LoaderOpenGL.h
@@ -5,6 +5,10 @@
 #include <CoreOpenGL/CoreOpenGL.h>
 #include <LoaderOpenGL/Defines.h>
 
+#include <string>
+#include <vector>
+#include <time.h>
+
 
 /***********************************************************************
  *
@@ -18,7 +22,6 @@ public:
 	enum LoaderOpenGLActions
 	{
 		UNKNOWN_ACTION = 0,
-		LOAD_MODEL,
 		STATUSBAR_VALUE,
 		STATUSBAR_COLOR,
 		STATUSBAR_VISIBILITY,
@@ -34,17 +37,52 @@ public:
 	virtual const char* className() { return OSG4WEB_LOADEROPENGL_LIBNAME; };
 	//Return Class Version
 	virtual const char* classVersion() { return OSG4WEB_LOADEROPENGL_VERSION; };
-
 	//Inizializza le Opzioni del core successivamente all'inizializzazione
 	virtual void AddStartOptions(std::string str, bool erase = true) { }
-
 	//Ridefinizioni del Gestore dei Comandi
 	virtual std::string handleAction(std::string argument);
 
 protected:
+	//OpenGL Initialization
+	virtual void initializeOpenGL();
+	//OpenGL Window Resize
+	virtual bool windowResize(int windowX, int windowY, int windowWidth, int windowHeight);
+	//OpenGL Rendering Scene
+	virtual bool renderImplementation();
 
+	bool loadTexture(std::string filename, GLuint texturelist);
 
+	bool loadTextureJPEG(std::string filename, GLuint texturelist);
+	//bool loadTextureGIF(std::string filename, GLuint texturelist);
 
+	bool handleShellMessages(std::string message);
+	bool refreshStatusBarValue(std::string value);
+
+	void createLines();
+	void createLogoTextureQuad();
+	void createTitleTextureQuad();
+	void createMessageTextureQuad();
+
+	void drawStatusBar();
+
+	float _pbarrdownload, _pbarrunpack, _pbarrload;
+	bool _showmessages;
+
+	unsigned int _message_id;
+	unsigned int _session;
+
+	GLuint _line_list;
+	
+	GLuint _logo_list;
+	GLuint _title_list;
+	GLuint _message_list;
+
+	GLuint _logo_texture_list;
+	GLuint _title_texture_list;
+	GLuint _message_texture_list;
+
+	clock_t _saved_timer;
+	unsigned int _image_tick;
 };
 
 #endif //__OSG4WEB_LOADEROPENGL__
