904413775663b632159bfe206311777b7c13c9a3
luigi.calori <luigi.calori@363b1339-bb4f-0410-9e72-7d383497cecc>
2009-10-10 09:22:47 +0000

    - Edit Manipulator Example - Aggiunto PickEditHandler a libreria SceneHandlers

---
+++ osg4web/bzr_import/src/Examples/CoreEdit/src/CoreEditExport.cpp
@@ -0,0 +1,10 @@
+
+#include <CoreEdit.h>
+
+using namespace OSG4WebCC;
+
+/** Template di allocazione del CoreEdit */
+CREATECLASSINSTANCE(CoreInterface, CoreEdit)
+/** Template di distruzione del CoreEdit */
+DELETECLASSINSTANCE(CoreInterface, CoreEdit)
+
